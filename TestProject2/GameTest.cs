033d37436b529e0b1c023d903001d27daeb97011
phymoond <phymoond@gmail.com>
2012-04-19 11:29:55 +0800

    Signed-off-by: phymoond <phymoond@gmail.com>

---
+++ TestProject2/GameTest.cs
@@ -0,0 +1,132 @@
+﻿//using NUnit.Framework;
+using BowlingGame;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+[TestClass]
+public class GameTest
+{
+    private Game game;
+
+    
+    public  GameTest ()
+    {
+       game = new Game(); 
+    }
+
+    [TestMethod]
+    public void TestTwoThrowsNoMark()
+    {
+        game.Add(5);
+        game.Add(4);
+        Assert.AreEqual(9, game.Score);
+    }
+
+    [TestMethod]
+    public void TestFourThrowsNoMark()
+    {
+        game.Add(5);
+        game.Add(4);
+        game.Add(7);
+        game.Add(2);
+        Assert.AreEqual(18, game.Score);
+        Assert.AreEqual(9, game.ScoreForFrame(1));
+        Assert.AreEqual(18, game.ScoreForFrame(2));
+    }
+
+    [TestMethod]
+    public void TestSimpleSpare()
+    {
+        game.Add(3);
+        game.Add(7);
+        game.Add(3);
+        Assert.AreEqual(13, game.ScoreForFrame(1));
+    }
+
+    [TestMethod]
+    public void TestSimpleFrameAfterSpare()
+    {
+        game.Add(3);
+        game.Add(7);
+        game.Add(3);
+        game.Add(2);
+        Assert.AreEqual(13, game.ScoreForFrame(1));
+        Assert.AreEqual(18, game.ScoreForFrame(2));
+        Assert.AreEqual(18, game.Score);
+    }
+
+    [TestMethod]
+    public void TestSimpleStrike()
+    {
+        game.Add(10);
+        game.Add(3);
+        game.Add(6);
+        Assert.AreEqual(19, game.ScoreForFrame(1));
+        Assert.AreEqual(28, game.Score);
+    }
+
+ 
+
+    [TestMethod]
+    public void TestEndOfArray()
+    {
+        for (int i=0; i<9; i++)
+        {
+            game.Add(0);
+            game.Add(0);
+        }
+        game.Add(2);
+        game.Add(8);
+        game.Add(10);
+        Assert.AreEqual(20, game.Score);
+    }
+
+
+    [TestMethod]
+    public void TestSampleGame()
+    {
+        game.Add(1);
+        game.Add(4);
+        game.Add(4);
+        game.Add(5);
+        game.Add(6);
+        game.Add(4);
+        game.Add(5);
+        game.Add(5);
+        game.Add(10);
+        game.Add(0);
+        game.Add(1);
+        game.Add(7);
+        game.Add(3);
+        game.Add(6);
+        game.Add(4);
+        game.Add(10);
+        game.Add(2);
+        game.Add(8);
+        game.Add(6);
+        Assert.AreEqual(133, game.Score);
+    }
+
+    [TestMethod]
+    public void TestHeartBreak()
+    {
+        for (int i=0; i<11; i++)
+        {
+            game.Add(10);
+        }
+        game.Add(9);
+        Assert.AreEqual(299, game.Score);
+    }
+
+    [TestMethod]
+    public void TestTenthFrameSpare()
+    {
+        for (int i=0; i<9; i++)
+        {
+            game.Add(10);
+        }
+        game.Add(9);
+        game.Add(1);
+        game.Add(1);
+        Assert.AreEqual(270, game.Score);
+    }
+}
